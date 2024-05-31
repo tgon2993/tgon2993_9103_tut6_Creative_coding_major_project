a2eba87056dc1b9065ad3fe8e4dfe2a3b6ac4df5
tgon2993 <tgon2993@uni.sydney.edu.au>
2024-05-31 10:44:40 +1000

    Update sketch.js
    I added a trigger event for the apple tree. Trigger events that appear one by one after a mouse click.

--- sketch.js
+++ sketch.js
@@ -1,45 +1,21 @@
+let colors = ['#6F8F6A', '#C15B5C', '#F8E8B6'];
+let currentColorIndex = 0;
+let rotationAngle = 0;
+let showElements = [];
+let totalElements = 25; // Total number of elements to show
+let currentElementIndex = 0;
+let rotationEnabled = false;
+
 function setup() {
   createCanvas(600, 800);
-  background(148, 177, 169); 
+  background(148, 177, 169);
 
-  // Draw the background rectangle with a different color
   drawBackgroundRect(50, 50, width - 100, height - 100, color(49, 74, 85));
 
   // Green & Red
-  let green = '#6F8F6A';
-   
-  let red = '#C15B5C';
-  let yellow = '#F8E8B6';
-
-  // Draw a circle divided up and down
-  function drawSplitCircle(x, y, diameter) {
-    fill(green);
-    arc(x, y, diameter, diameter, PI, 0);
-    fill(red);
-    arc(x, y, diameter, diameter, 0, PI);
-    noFill();
-    ellipse(x, y, diameter, diameter);
-  }
-  
-  // Draw a circle divided right and left
-  function drawSplitCircleLR(x, y, diameter) {
-    fill(green);
-    arc(x, y, diameter, diameter, HALF_PI, HALF_PI + PI);
-    fill(red);
-    arc(x, y, diameter, diameter, HALF_PI + PI, HALF_PI);
-    noFill();
-    ellipse(x, y, diameter, diameter);
-  }
-
-  // Draw a circle divided by red and green
-  function drawSplitCircleTopRed(x, y, diameter) {
-    fill(red);
-    arc(x, y, diameter, diameter, PI, 0);
-    fill(green);
-    arc(x, y, diameter, diameter, 0, PI);
-    noFill();
-    ellipse(x, y, diameter, diameter);
-  }
+  let green = colors[currentColorIndex % colors.length];
+  let red = colors[(currentColorIndex + 1) % colors.length];
+  let yellow = colors[(currentColorIndex + 2) % colors.length];
 
   // Central position
   let centerX = width / 2;
@@ -48,82 +24,75 @@ function setup() {
   // List of diameters of circles
   let diameters = [80, 50, 30, 60];
 
-  // Central circle
-  drawSplitCircle(centerX, centerY, diameters[0]);
-
-  // Left arm circle
-  drawSplitCircle(centerX - 0.82 * diameters[0], centerY, diameters[1]);
-  drawSplitCircle(centerX - 0.7 * diameters[0] - diameters[1], centerY, diameters[2]);
-  drawSplitCircleLR(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2, centerY - 2 * diameters[2] / 2, diameters[3]);
-
-  // Right arm circle
-  drawSplitCircle(centerX + 0.88 * diameters[0], centerY, diameters[3]);
-  drawSplitCircle(centerX + 1.26 * diameters[0] + diameters[2], centerY, diameters[3]);
-  drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2,centerY,centerX + 1.26 * diameters[0] + diameters[2]+diameters[3]/2,centerY)
-  drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2,centerY - 2 * diameters[2] / 2+diameters[3]/2,centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2,centerY - 2 * diameters[2] / 2-diameters[3]/2)
-
-  // List of diameters of circles
-  diameters = [60, 50, 80, 30];
-
-  push();
-  // Draw the bottom rectangle here
-  drawBottomRectangles(centerX, centerY + 1.11 * diameters[1] + diameters[2] + diameters[3], 300, 50, green, yellow);
-
-    // Draw a small green rectangle over the yellow rectangle
-    fill("#6F8F6A"); 
-    noStroke();
-    rect(centerX - 105, centerY + 1.7 * diameters[1] + diameters[2] + diameters[3], 50, 60);
-    
-    // Draw a small yellow circle on the small green rectangle
-    fill("#F8E8B6"); 
-    noStroke();
-    ellipse(centerX - 80, centerY + 2.3 * diameters[1] + diameters[2] + diameters[3], 50);
-  
-     // Draw a small red rectangle over the yellow rectangle
-     fill("#C15B5C"); 
-     rect(centerX + 55, centerY + 1.7 * diameters[1] + diameters[2] + diameters[3], 60, 60);
-  
-    // Draw a small yellow circle over the small red rectangle
-    fill("#F8E8B6"); 
-    ellipse(centerX + 85, centerY + 2.2 * diameters[1] + diameters[2] + diameters[3], 60);
-  
-  pop();  
-
-  // Upper median radius
-  drawSplitCircleLR(centerX, centerY - 0.92 * diameters[0], diameters[3]);
-  drawSplitCircleLR(centerX, centerY - 1.68 * diameters[0], diameters[0]);
-
-  // Lower median radius
-  drawSplitCircleLR(centerX, centerY + 1.3 * diameters[1], diameters[1]);
-  drawSplitCircleLR(centerX, centerY + diameters[1] + diameters[2], diameters[2]);
-  drawSplitCircleLR(centerX, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[1]);
-  drawLine(centerX,centerY + 1.71 * diameters[1] + diameters[2] + diameters[3]+diameters[1]/2,centerX,centerY - 1.68 * diameters[0]-diameters[0]/2);
-
-  // Draw more middle bottom square, using red on top and green on bottom
-  drawSplitCircleTopRed(centerX + 0.8 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[3]);
-  drawSplitCircleTopRed(centerX + 1.7 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[0]);
-  drawSplitCircleTopRed(centerX - 0.8 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[3]);
-  drawSplitCircleTopRed(centerX - 1.6 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[1]);
-  drawLine(centerX - 1.6 * diameters[1] - diameters[1] / 2, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], centerX + 1.7 * diameters[1] + diameters[0] / 2, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3]);
-
-  // List of diameters of circles
-  diameters = [30, 50, 80, 60];
-
-  // Right side vertical circle
-  drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0]+diameters[3]/2, centerY - 1.8 * diameters[0]+diameters[2]*0.1, diameters[1]);
-  drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0]+diameters[3]/2, centerY - 1.48 * diameters[0] - diameters[1]+diameters[2]*0.1, diameters[0]);
-  drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0]+diameters[3]/2, centerY - 0.65 * diameters[0] - diameters[1] - diameters[2]+diameters[2]*0.1, diameters[2]);
-  drawLine(centerX + 1.26 * diameters[2] + diameters[0]+diameters[3]/2,centerY,centerX + 1.26 * diameters[2] + diameters[0]+diameters[3]/2,centerY - 0.65 * diameters[0] - diameters[1] - diameters[2]+diameters[2]*0.1-diameters[2]/2);
-
-  // Draw background texture
-  for (let i = 0; i < 10000; i++) {
-    stroke(200, 200, 250, 50);
-    point(random(width), random(height));
+  for (let i = 0; i < totalElements; i++) {
+    showElements.push(false);
   }
+
+  // Add event listener for mouse click
+  canvas.addEventListener('click', startAnimation);
 }
 
 function draw() {
-  // Empty draw function
+  // Clear the background for each frame
+  background(148, 177, 169);
+  drawBackgroundRect(50, 50, width - 100, height - 100, color(49, 74, 85));
+
+  // Green & Red
+  let green = colors[currentColorIndex % colors.length];
+  let red = colors[(currentColorIndex + 1) % colors.length];
+  let yellow = colors[(currentColorIndex + 2) % colors.length];
+
+  // Central position
+  let centerX = width / 2;
+  let centerY = 400;
+
+  // List of diameters of circles
+  let diameters = [80, 50, 30, 60];
+
+  if (showElements[0]) drawSplitCircle(centerX, centerY, diameters[0], green, red);
+  if (showElements[1]) drawSplitCircle(centerX - 0.82 * diameters[0], centerY, diameters[1], green, red);
+  if (showElements[2]) drawSplitCircle(centerX - 0.7 * diameters[0] - diameters[1], centerY, diameters[2], green, red);
+  if (showElements[3]) drawSplitCircleLR(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2, centerY - diameters[2], diameters[3], green, red);
+  if (showElements[4]) drawSplitCircle(centerX + 0.88 * diameters[0], centerY, diameters[3], green, red);
+  if (showElements[5]) drawSplitCircle(centerX + 1.26 * diameters[0] + diameters[2], centerY, diameters[3], green, red);
+  if (showElements[6]) drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2, centerY, centerX + 1.26 * diameters[0] + diameters[2] + diameters[3] / 2, centerY);
+  if (showElements[7]) drawLine(centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2, centerY - diameters[2] + diameters[3] / 2, centerX - 0.95 * diameters[0] - diameters[1] - diameters[2] / 2, centerY - diameters[2] - diameters[3] / 2);
+  if (showElements[8]) drawBottomRectangles(centerX, centerY + 1.11 * diameters[1] + diameters[2] + diameters[3], 300, 50, green, yellow);
+  if (showElements[9]) drawRectAndCircle(centerX - 105, centerY + 1.7 * diameters[1] + diameters[2] + diameters[3], 50, 60, green, yellow, centerX - 80, centerY + 2.3 * diameters[1] + diameters[2] + diameters[3], 50);
+  if (showElements[10]) drawRectAndCircle(centerX + 55, centerY + 1.7 * diameters[1] + diameters[2] + diameters[3], 60, 60, red, yellow, centerX + 85, centerY + 2.2 * diameters[1] + diameters[2] + diameters[3], 60);
+  if (showElements[11]) drawSplitCircleLR(centerX, centerY - 0.92 * diameters[0], diameters[3], green, red);
+  if (showElements[12]) drawSplitCircleLR(centerX, centerY - 1.68 * diameters[0], diameters[0], green, red);
+  if (showElements[13]) drawSplitCircleLR(centerX, centerY + 1.3 * diameters[1], diameters[1], green, red);
+  if (showElements[14]) drawSplitCircleLR(centerX, centerY + diameters[1] + diameters[2], diameters[2], green, red);
+  if (showElements[15]) drawSplitCircleLR(centerX, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[1], green, red);
+  if (showElements[16]) drawLine(centerX, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3] + diameters[1] / 2, centerX, centerY - 1.68 * diameters[0] - diameters[0] / 2);
+  if (showElements[17]) drawSplitCircleTopRed(centerX + 0.8 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[3], red, green);
+  if (showElements[18]) drawSplitCircleTopRed(centerX + 1.7 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[0], red, green);
+  if (showElements[19]) drawSplitCircleTopRed(centerX - 0.8 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[3], red, green);
+  if (showElements[20]) drawSplitCircleTopRed(centerX - 1.6 * diameters[1], centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], diameters[1], red, green);
+  if (showElements[21]) drawLine(centerX - 1.6 * diameters[1] - diameters[1] / 2, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3], centerX + 1.7 * diameters[1] + diameters[0] / 2, centerY + 1.71 * diameters[1] + diameters[2] + diameters[3]);
+  if (showElements[22]) drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2, centerY - 1.8 * diameters[0] + diameters[2] * 0.1, diameters[1], green, red);
+  if (showElements[23]) drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2, centerY - 1.48 * diameters[0] - diameters[1] + diameters[2] * 0.1, diameters[0], green, red);
+  if (showElements[24]) drawSplitCircleLR(centerX + 1.26 * diameters[2] + diameters[0] + diameters[3] / 2, centerY - 0.65 * diameters[0] - diameters[1] - diameters[2] + diameters[2] * 0.1, diameters[2], green, red);
+
+  // Rotate the circles if all elements have appeared
+  if (rotationEnabled) {
+    rotationAngle += 0.01; // Increase the rotation angle
+  }
+}
+
+function startAnimation() {
+  currentElementIndex = 0;
+  rotationEnabled = false;
+
+  let interval = setInterval(() => {
+    showElements[currentElementIndex] = true;
+    currentElementIndex++;
+    if (currentElementIndex >= totalElements) {
+      clearInterval(interval);
+      rotationEnabled = true; // Enable rotation after all elements are shown
+    }
+  }, 500);
 }
 
 function drawBackgroundRect(x, y, width, height, color) {
@@ -143,11 +112,63 @@ function drawBottomRectangles(centerX, y, rectWidth, rectHeight, green, yellow) 
   rect(startX + rectWidth, y + 30, rectWidth, rectHeight + 10);
 }
 
-function drawLine(x,y,x1,y1){
+function drawLine(x, y, x1, y1) {
   push();
-  stroke(235,187,138)
-  strokeWeight(3)
-  line(x,y,x1,y1)
+  stroke(235, 187, 138);
+  strokeWeight(3);
+  line(x, y, x1, y1);
   pop();
+}
+
+function drawSplitCircle(x, y, diameter, color1, color2) {
+  push();
+  translate(x, y);
+  if (rotationEnabled) {
+    rotate(rotationAngle);
+  }
+  fill(color1);
+  arc(0, 0, diameter, diameter, 0, PI);
+  fill(color2);
+  arc(0, 0, diameter, diameter, PI, 0);
+  noFill();
+  ellipse(0, 0, diameter, diameter);
+  pop();
+}
+
+function drawSplitCircleLR(x, y, diameter, color1, color2) {
+  push();
+  translate(x, y);
+  if (rotationEnabled) {
+    rotate(rotationAngle);
+  }
+  fill(color1);
+  arc(0, 0, diameter, diameter, -HALF_PI, HALF_PI);
+  fill(color2);
+  arc(0, 0, diameter, diameter, HALF_PI, -HALF_PI);
+  noFill();
+  ellipse(0, 0, diameter, diameter);
+  pop();
+}
+
+function drawSplitCircleTopRed(x, y, diameter, color1, color2) {
+  push();
+  translate(x, y);
+  if (rotationEnabled) {
+    rotate(rotationAngle);
+  }
+  fill(color1);
+  arc(0, 0, diameter, diameter, 0, PI);
+  fill(color2);
+  arc(0, 0, diameter, diameter, PI, 0);
+  noFill();
+  ellipse(0, 0, diameter, diameter);
+  pop();
+}
+
+function drawRectAndCircle(rectX, rectY, rectWidth, rectHeight, rectColor, circleColor, circleX, circleY, circleDiameter) {
+  fill(rectColor);
+  rect(rectX, rectY, rectWidth, rectHeight);
+  fill(circleColor);
+  ellipse(circleX, circleY, circleDiameter, circleDiameter);
 }
 
